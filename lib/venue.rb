183cc70b4c74ed7a914eea7f33efb45faa96d028
Annie Pulzone <ajpulzone@gmail.com>
2022-08-24 08:38:14 -0700

    pass yell at patrons test

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,13 +1,21 @@
 class Venue
-  attr_reader :name, :capacity, :patrons
+  attr_reader :name, :capacity, :patrons, :yelled_at_patrons
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
     @patrons = []
+    @yelled_at_patrons = []
   end
 
   def add_patron(patron)
     @patrons << patron
-  end 
+  end
+
+  def yell_at_patrons
+    patrons.each do |patron|
+        yelled_at_patrons << patron.upcase
+    end
+    return yelled_at_patrons ##Might have to rework this moving forward, but works for now.
+  end
 end
